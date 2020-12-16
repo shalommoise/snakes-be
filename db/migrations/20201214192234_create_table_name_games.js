b927a547c5f916ab2bad53450c1d9730b43aab8e
Shalommoise <mshalom689@gmail.com>
2020-12-16 17:51:11 +0000

    table missing cooridnates for snakes

--- db/migrations/20201214192234_create_table_name_games.js
+++ db/migrations/20201214192234_create_table_name_games.js
@@ -1,11 +1,13 @@
-
+const {passwordGenorator} = require("../../utils/utils")
 exports.up = function(knex) {
     return knex.schema.createTable("games", (gamesTable) => {
     gamesTable.increments("game_id").primary();
-    gamesTable.integer("login_code").defaultsTo(randNumFunc());
-    gamesTable.string("player1");
-    gamesTable.string("player2");
-  
+    gamesTable.integer("login_code").defaultsTo(new passwordGenorator());
+    gamesTable.string("player1").notNullable();
+    gamesTable.string("player2").defaultsTo("player2");
+    gamesTable.string("snake1").defaultsTo([[],344]);
+    gamesTable.string("snake2").defaultsTo([[],24]);
+    gamesTable.timestamp("created_at").defaultTo(knex.fn.now());
   });
 };
 
