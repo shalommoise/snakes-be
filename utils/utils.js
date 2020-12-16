685af7aad56fe26a6717c324995772aac3ffb689
Shalommoise <mshalom689@gmail.com>
2020-12-16 17:41:40 +0000

    passwordgenoarto complete

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,10 @@
 const passwordGenorator =()=>{
 
-const two = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-const three = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  let code = "$"+ two+ three + 34;
+const secondChar = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+const thirdChar = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+const fourthChar = Math.floor(10 + Math.random() * 90);
+
+const code = "$"+ secondChar+ thirdChar + fourthChar;
 
   return code
 };
