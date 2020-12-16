8da336fb9b296192c5cb816ceb2f4279af65171c
Shalommoise <mshalom689@gmail.com>
2020-12-16 17:04:46 +0000

    wrtitng schema for game

--- db/migrations/20201214192234_create_table_name_games.js
+++ db/migrations/20201214192234_create_table_name_games.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable("games", (gamesTable) => {
+    gamesTable.increments("game_id").primary();
+    gamesTable.integer("login_code").defaultsTo(randNumFunc());
+    gamesTable.string("player1");
+    gamesTable.string("player2");
   
+  });
 };
 
 exports.down = function(knex) {
-  
+   return knex.schema.dropTable("games");
 };
