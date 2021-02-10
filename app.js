af953661ee07213d5c904896cf5c95896c28e71d
Shalommoise <mshalom689@gmail.com>
2021-02-10 12:10:44 +0000

    moved cors

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const cors = require("cors");
 const {serverErrorHandler} = require("./errors");
 const apiRouter = require("./routers/api.router");
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
-app.use(cors());
+
 app.all("*", (req, res, next) => {
   res.status(404).send({ msg: "404 Error: Path Not found" });
 });
