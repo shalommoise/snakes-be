6e9f9b8c2c68348151c6165bc733e021c9b790a5
Shalommoise <mshalom689@gmail.com>
2021-02-17 12:50:12 +0000

    removed comment

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,11 @@
 const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
-const config = require("../knexfile");
+
 
 const dbConfig =
-// ENV === "development"
-  // ENV === "test"
+
   ENV === "production"
     ? { client: "pg", connection: process.env.DATABASE_URL }
-    : config;
+    : require("../knexfile");
 
 module.exports = knex(dbConfig);
